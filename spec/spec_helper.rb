485d51cb52032729e2fc844008386671be5545f0
Rafał Michalski <royaltm75@gmail.com>
2014-01-04 20:23:50 +0100

    spec: COVERAGE control env variable

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
-require 'simplecov'
-require 'coveralls'
+if ENV["COVERAGE"]
+  require 'simplecov'
+  require 'coveralls'
 
-SimpleCov.start do
-  add_filter "/spec/"
+  SimpleCov.start do
+    add_filter "/spec/"
+  end
+  SimpleCov.command_name ENV['COVNAME'] || 'RSpec'
 end
-SimpleCov.command_name ENV['COVNAME'] || 'RSpec'
