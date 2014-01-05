d5133bac45819a3b3223ae3807e31043946e93c1
Rafał Michalski <royaltm75@gmail.com>
2014-01-05 01:09:56 +0100

    examples: single row mode

---
+++ examples/single_row_mode.rb
@@ -0,0 +1,59 @@
+# Demonstation of fully asynchronous query data streaming.
+#
+# This is a low-level method which has its:
+#
+# upside:   it's the same way you would work with PG::Connection
+# downside: it's a little verbose and doesn't support automatic re-connects
+gem 'em-pg-client', '>= 0.3.1'
+require 'pg/em/connection_pool'
+require 'em-synchrony'
+require 'em-synchrony/fiber_iterator'
+
+TABLE_NAME = 'resources'
+
+unless PG::EM::Client.single_row_mode?
+  raise 'compile pg against pqlib >= 9.2 to support single row mode'
+end
+
+EM.synchrony do
+  EM.add_periodic_timer(0.01) { print ' ' }
+
+  db = PG::EM::ConnectionPool.new size: 2
+
+  10.times do
+
+    EM::Synchrony::FiberIterator.new(%w[@ *], 2).each do |mark|
+
+      db.hold do |pg|
+# PG::Connection compatible code starts here
+        pg.send_query("select * from #{TABLE_NAME}")
+        pg.set_single_row_mode
+        rows = 0
+        while result = pg.get_result
+          begin
+            result.check
+            result.each_with_index do |tuple|
+              rows += 1
+              # process tuple
+              print mark
+              # break stream cleanly
+              pg.reset if rows > 1000
+            end
+          rescue PG::Error => e
+            # cleanup connection
+            pg.get_last_result
+            raise e
+          ensure
+            result.clear
+          end
+        end
+# PG::Connection compatible code ends
+      end
+
+    end
+
+    puts
+    puts '='*80
+  end
+  EM.stop
+end
